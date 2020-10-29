87f45a9c919d418c1b8254df1756649cb32c0034
DESKTOP-FLG6SMB\Benoît <benoit.delecluse@satellit.be>
2020-10-29 16:51:53 +0100

    Exercice 3 version 6

--- Exercice3Library/TxtFile.cs
+++ Exercice3Library/TxtFile.cs
@@ -5,20 +5,28 @@ namespace Exercice3Library
 {
     public class TxtFile
     {
+        private Security.SecurityContext Role;
+        public TxtFile(Security.SecurityContext role)
+        {
+            Role = role;
+        }
         public string ReadTxtFile(string path)
         {
-            if (System.IO.File.Exists(path))
+            if (this.Role.CheckRolesForFile(path))
             {
-                return System.IO.File.ReadAllText(path);
+                if (System.IO.File.Exists(path))
+                {
+                    return System.IO.File.ReadAllText(path);
+                }
+                throw new Exception("File does not exist");
             }
-            throw new Exception("File does not exist");
-
+            throw new Exception("Access to the file : " + path + "  is disabled due to role restriction.");
         }
 
-        public string ReadCryptedTextFile(string path , ITextCrypted Algo)
+        public string ReadCryptedTextFile(string path, ITextCrypted Algo)
         {
             var cryptedValue = ReadTxtFile(path);
-            return Algo.Uncrypte(cryptedValue); 
+            return Algo.Uncrypte(cryptedValue);
         }
     }
 }
