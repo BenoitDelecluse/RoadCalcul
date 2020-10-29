72f487f221da08eed924d4a4eaf76d09d1b44101
DESKTOP-FLG6SMB\Benoît <benoit.delecluse@satellit.be>
2020-10-29 12:19:31 +0100

    Exercice 3 version 2

---
+++ Exercice3Library/XmlFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace Exercice3Library
+{
+    public class XmlFile
+    {
+        public XmlDocument ReadXmlFile(string path)
+        {
+            if (System.IO.File.Exists(path))
+            {
+                XmlDocument xmldoc = new XmlDocument();
+                FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+                xmldoc.Load(fs);
+            }
+            throw new Exception("File does not exist");
+        }
+    }
+}
