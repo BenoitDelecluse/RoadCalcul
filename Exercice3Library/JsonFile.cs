727f6de6eb5044c80d1403486c7fa85b9e03cf20
DESKTOP-FLG6SMB\Benoît <benoit.delecluse@satellit.be>
2020-10-29 17:26:04 +0100

    Exercice 3 version 7

---
+++ Exercice3Library/JsonFile.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Exercice3Library
+{
+    public class JsonFile
+    {
+        public string ReadJsonFile(string path)
+        {
+            if (System.IO.File.Exists(path))
+            {
+                var st = System.IO.File.ReadAllText(path);
+                try 
+                {
+                    var result = JsonConvert.DeserializeObject(st);
+                }
+                catch(Exception ex)
+                {
+                    throw new Exception(path + " is not a json file" + ex.Message);
+                }
+                return st;
+            }
+            throw new Exception("File does not exist");
+        }
+    }
+}
