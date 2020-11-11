a8e812c820f303866a5cc9fc8189f93757976a56
DESKTOP-FLG6SMB\Benoît <benoit.delecluse@satellit.be>
2020-11-11 09:12:40 +0100

    Exercice 3 Version 8.0

--- Exercice3Library/JsonFile.cs
+++ Exercice3Library/JsonFile.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Exercice3Library.Algorithm;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -24,6 +25,30 @@ namespace Exercice3Library
                 return st;
             }
             throw new Exception("File does not exist");
+        }
+
+        public String ReadCryptedJsonlFile(string path, ITextCrypted Algo)
+        {
+            if (System.IO.File.Exists(path))
+            {
+                var cryptedValue = System.IO.File.ReadAllText(path);
+                var uncrypted = Algo.Uncrypte(cryptedValue);
+                try
+                {
+                    var result = JsonConvert.DeserializeObject(uncrypted);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(path + " is not a json file" + ex.Message);
+                }
+                return uncrypted;
+            }
+            throw new Exception("File does not exist");
+        }
+
+        private String handleJason(string value, ITextCrypted Algo)
+        {
+            return value;
         }
     }
 }
