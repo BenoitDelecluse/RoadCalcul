361f08b128d8e73508f6f110a96970a3628004f3
DESKTOP-FLG6SMB\Benoît <benoit.delecluse@satellit.be>
2020-11-11 09:31:01 +0100

    Exercicie 3 version 9

--- Exercice3Library/JsonFile.cs
+++ Exercice3Library/JsonFile.cs
@@ -9,46 +9,55 @@ namespace Exercice3Library
 {
     public class JsonFile
     {
+        private Security.SecurityContext Role;
+        public JsonFile(Security.SecurityContext role)
+        {
+            Role = role;
+        }
+     
         public string ReadJsonFile(string path)
         {
-            if (System.IO.File.Exists(path))
+            if (this.Role.CheckRolesForFile(path))
             {
-                var st = System.IO.File.ReadAllText(path);
-                try 
+                if (System.IO.File.Exists(path))
                 {
-                    var result = JsonConvert.DeserializeObject(st);
+                    var st = System.IO.File.ReadAllText(path);
+                    try
+                    {
+                        var result = JsonConvert.DeserializeObject(st);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(path + " is not a json file" + ex.Message);
+                    }
+                    return st;
                 }
-                catch(Exception ex)
-                {
-                    throw new Exception(path + " is not a json file" + ex.Message);
-                }
-                return st;
+                throw new Exception("File does not exist");
             }
-            throw new Exception("File does not exist");
+            throw new Exception("Access to the file : " + path + "  is disabled due to role restriction.");
         }
 
         public String ReadCryptedJsonlFile(string path, ITextCrypted Algo)
         {
-            if (System.IO.File.Exists(path))
+            if (this.Role.CheckRolesForFile(path))
             {
-                var cryptedValue = System.IO.File.ReadAllText(path);
-                var uncrypted = Algo.Uncrypte(cryptedValue);
-                try
+                if (System.IO.File.Exists(path))
                 {
-                    var result = JsonConvert.DeserializeObject(uncrypted);
+                    var cryptedValue = System.IO.File.ReadAllText(path);
+                    var uncrypted = Algo.Uncrypte(cryptedValue);
+                    try
+                    {
+                        var result = JsonConvert.DeserializeObject(uncrypted);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(path + " is not a json file" + ex.Message);
+                    }
+                    return uncrypted;
                 }
-                catch (Exception ex)
-                {
-                    throw new Exception(path + " is not a json file" + ex.Message);
-                }
-                return uncrypted;
+                throw new Exception("File does not exist");
             }
-            throw new Exception("File does not exist");
-        }
-
-        private String handleJason(string value, ITextCrypted Algo)
-        {
-            return value;
+            throw new Exception("Access to the file : " + path + "  is disabled due to role restriction."); 
         }
     }
 }
